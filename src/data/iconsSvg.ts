efd42223f83fefe8c7e617bba31d617e88fdca84
FedericoHernanSanchez <federicohernansanchezof@gmail.com>
2025-11-14 18:03:56 -0300

    fix

--- src/data/iconsSvg.ts
+++ src/data/iconsSvg.ts
@@ -1,43 +1,22 @@
-import Html from "../assets/icons/html.svg?url";
-import Css from "../assets/icons/css.svg?url";
-import React from "../assets/icons/react.svg?url";
-import Tailwind from "../assets/icons/tailwind.svg?url";
-import Javascript from "../assets/icons/javascript.svg?url";
-import Typescript from "../assets/icons/typescript.svg?url";
-import Astro from "../assets/icons/astro.svg?url";
-import NodeJs from "../assets/icons/nodejs.svg?url";
-import MySqL from "../assets/icons/mysql.svg?url";
-import Git from "../assets/icons/git.svg?url";
-import World from "../assets/icons/world.svg?url";
-import Github from "../assets/icons/github.svg?url";
-import House from "../assets/icons/house.svg?url";
-import Study from "../assets/icons/study.svg?url";
-import Code from "../assets/icons/code.svg?url";
-import Send from "../assets/icons/send.svg?url";
-import User from "../assets/icons/user.svg?url";
-import Menu from "../assets/icons/menu.svg?url";
-import Download from "../assets/icons/download.svg?url";
-import Linkedin from "../assets/icons/linkedin.svg?url";
-
-export const iconsSvg: Record<string, string> = {
-  HTML: Html,
-  CSS: Css,
-  React: React,
-  Tailwind: Tailwind,
-  JavaScript: Javascript,
-  TypeScript: Typescript,
-  Astro: Astro,
-  NodeJs: NodeJs,
-  MySQL: MySqL,
-  Git: Git,
-  world: World,
-  github: Github,
-  house: House,
-  study: Study,
-  code: Code,
-  send: Send,
-  user: User,
-  menu: Menu,
-  download: Download,
-  linkedin: Linkedin,
+export const iconsSvg = {
+  HTML: "/icons/html.svg",
+  CSS: "/icons/css.svg",
+  React: "/icons/react.svg",
+  Tailwind: "/icons/tailwind.svg",
+  JavaScript: "/icons/javascript.svg",
+  TypeScript: "/icons/typescript.svg",
+  Astro: "/icons/astro.svg",
+  NodeJs: "/icons/nodeJs.svg",
+  MySQL: "/icons/mySql.svg",
+  Git: "/icons/git.svg",
+  world: "/icons/world.svg",
+  github: "/icons/github.svg",
+  house: "/icons/house.svg",
+  study: "/icons/study.svg",
+  code: "/icons/code.svg",
+  send: "/icons/send.svg",
+  user: "/icons/user.svg",
+  menu: "/icons/menu.svg",
+  download: "/icons/download.svg",
+  linkedin: "/icons/linkedIn.svg",
 };
