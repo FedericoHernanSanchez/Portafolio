1cc65125687368fed41797d98f0005cf7e153a5e
Federico Sanchez <federicohernansanchezof@gmail.com>
2026-07-08 15:34:40 -0300

    fix proyectos

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -10,10 +10,11 @@ export const projects = [
   {
     image: "/underground.png",
     title: "Underground",
-    description: "Tienda de ropa desarrollada con React.js que incluye carrito de compras para usuarios y un panel de gestión de productos para el administrador. Proyecto realizado para el curso React.js de Talento Tech.",
+    description: "Tienda  desarrollada con React.js que incluye un carrito de compras para los usuarios y un panel de administración para la gestión de productos. Los datos de los productos se obtienen mediante una API simulada utilizando MockAPI.",
     technologies: ["HTML", "CSS","React"],
     githubUrl: "https://github.com/FedericoHernanSanchez/Proyecto.React",
-    liveUrl: "https://react-proyect-23242c.netlify.app/"
+    liveUrl: "https://react-proyect-23242c.netlify.app/",
+    status: "En progreso"
   },
   {
     image: "/cientifica.png",
@@ -21,7 +22,8 @@ export const projects = [
     description: "Página de ventas de productos para laboratorios desarrollada con WordPress y Elementor durante una pasantía en la agencia Lader para un cliente del sector científico.",
     technologies: ["Wordpress", "Elementor"],
     githubUrl: null,
-    liveUrl: "https://cientificacentra.lader.com.ar/"
+    liveUrl: "https://cientificacentra.lader.com.ar/",
+    status: "Finalizado"
   }
   
 ];
